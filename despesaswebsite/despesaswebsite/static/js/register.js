92394a26dab0b79c7a1197c8724582719e038e8f
Geovani Paz <geovanipaz7@gmail.com>
2022-09-02 14:34:50 -0300

    validação de username com js

--- despesaswebsite/despesaswebsite/static/js/register.js
+++ despesaswebsite/despesaswebsite/static/js/register.js
@@ -1,5 +1,28 @@
-const usernameField = document.querySelector('#usernameField')
+const usernameField = document.querySelector("#usernameField");
+const feedBackArea = document.querySelector(".invalid_feedback");
+//'keyup' ação de teclado
+usernameField.addEventListener("keyup", (e) => {
+    console.log('7777', 8888);
 
-usernameField.addEventListener("keyup", () => {
-    console.log('7777', 8888)
-})
+    const usernameVal = e.target.value;
+
+    usernameField.classList.remove("is-valid");
+    feedBackArea.style.display = "none"
+
+    if(usernameVal.length > 0){
+        fetch("/autenticacao/validar-username", {
+            body:JSON.stringify({username: usernameVal}),
+            method:"POST",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data", data);
+            if (data.username_erro){
+                usernameField.classList.add("is-valid");
+                feedBackArea.style.display = "block"
+                feedBackArea.innerHTML = `<p>${data.username_erro}</p>`
+            }
+        });
+    }
+    
+});
