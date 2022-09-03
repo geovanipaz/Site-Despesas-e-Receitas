215ff10b2a50dce98670d3f1c0172a39ae21d2af
Geovani Paz <geovanipaz7@gmail.com>
2022-09-03 14:30:34 -0300

    validação do 'username'

--- despesaswebsite/authentication/urls.py
+++ despesaswebsite/authentication/urls.py
@@ -1,9 +1,11 @@
-from .views import RegistrationView, UsernameValidationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('registrar', RegistrationView.as_view(), name='registrar'),
     path('validar-username', csrf_exempt(UsernameValidationView.as_view())
-         , name='validar-username')
+         , name='validar-username'),
+    path('validar-email', csrf_exempt(EmailValidationView.as_view()),
+         name='validar-email')
 ]
--- despesaswebsite/authentication/views.py
+++ despesaswebsite/authentication/views.py
@@ -4,7 +4,27 @@ from django.views import View
 from django.contrib.auth.models import User
 import json
 from django.http import JsonResponse
+from django.core.validators import validate_email, EmailValidator
+from django.core.exceptions import ValidationError
+#from validate_email import validate_email
 # Create your views here.
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        
+        #passa se email é válido
+        try:
+            validate_email(email)
+        except ValidationError:
+            return JsonResponse({'email_erro':'Email inválido'
+                                 ''}, status=400)
+            
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_erro':'Email já está sendo usado. '
+                                 'Escolha outro'}, status=409)
+        return JsonResponse({'email_valid': True})
 
 class UsernameValidationView(View):
     def post(self, request):
