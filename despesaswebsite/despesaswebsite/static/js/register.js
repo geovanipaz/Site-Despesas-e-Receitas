213381463c1b67f37e5fb66815023a053c9bc289
Geovani Paz <geovanipaz7@gmail.com>
2022-09-04 14:22:58 -0300

    validação de email

--- despesaswebsite/despesaswebsite/static/js/register.js
+++ despesaswebsite/despesaswebsite/static/js/register.js
@@ -1,10 +1,42 @@
 const usernameField = document.querySelector("#usernameField");
 const feedBackArea = document.querySelector(".invalid_feedback");
+const emailField = document.querySelector("#emailField")
+const  emailFeedBackArea = document.querySelector(".emailFeedBackArea")
+const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput')
+
+
+emailField.addEventListener("keyup", (e) => {
+    const emailVal = e.target.value;
+
+    emailField.classList.remove("is-valid");
+    emailFeedBackArea.style.display = "none"
+
+    if(emailVal.length > 0){
+        fetch("/autenticacao/validar-email", {
+            body:JSON.stringify({email: emailVal}),
+            method:"POST",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data", data);
+            if (data.email_erro){
+                emailField.classList.add("is-valid");
+                emailFeedBackArea.style.display = "block"
+                emailFeedBackArea.innerHTML = `<p>${data.email_erro}</p>`
+            }
+        });
+    }
+})
+
+
+
 //'keyup' ação de teclado
 usernameField.addEventListener("keyup", (e) => {
-    console.log('7777', 8888);
-
+    //console.log('7777', 8888);
+    
     const usernameVal = e.target.value;
+
+    usernameSuccessOutput.textContent=`Checking ${usernameVal}`
 
     usernameField.classList.remove("is-valid");
     feedBackArea.style.display = "none"
@@ -17,6 +49,7 @@ usernameField.addEventListener("keyup", (e) => {
         .then((res) => res.json())
         .then((data) => {
             console.log("data", data);
+            usernameSuccessOutput.style.display="none";
             if (data.username_erro){
                 usernameField.classList.add("is-valid");
                 feedBackArea.style.display = "block"
