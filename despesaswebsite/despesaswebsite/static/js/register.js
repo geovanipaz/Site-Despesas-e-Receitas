02dea02079994c02a0c16e781267a7fdea8b2ad4
Geovani Paz <geovanipaz7@gmail.com>
2022-09-05 10:09:24 -0300

    mostrar/esconder campo de senha

--- despesaswebsite/despesaswebsite/static/js/register.js
+++ despesaswebsite/despesaswebsite/static/js/register.js
@@ -3,7 +3,20 @@ const feedBackArea = document.querySelector(".invalid_feedback");
 const emailField = document.querySelector("#emailField")
 const  emailFeedBackArea = document.querySelector(".emailFeedBackArea")
 const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput')
+const showPasswordToggle = document.querySelector('.showPasswordToggle')
+const passwordField = document.querySelector('#passwordField')
 
+handleToggleInput = (e) =>{
+    if(showPasswordToggle.textContent === "MOSTRAR"){
+        showPasswordToggle.textContent = "ESCONDER"
+        passwordField.setAttribute("type", "text");
+    }else{
+        showPasswordToggle.textContent = "MOSTRAR"
+        passwordField.setAttribute("type", "password");
+    }
+};
+
+showPasswordToggle.addEventListener("click", handleToggleInput);
 
 emailField.addEventListener("keyup", (e) => {
     const emailVal = e.target.value;
