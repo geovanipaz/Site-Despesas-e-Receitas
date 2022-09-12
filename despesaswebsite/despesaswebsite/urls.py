5c324f21ee4cc9de3b5215e392da1a34f267450e
Geovani Paz <geovanipaz7@gmail.com>
2022-09-11 21:41:45 -0300

    escolha da moeda preferida

--- despesaswebsite/despesaswebsite/urls.py
+++ despesaswebsite/despesaswebsite/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('expenses.urls')),
-    path('autenticacao/', include('authentication.urls'))
+    path('autenticacao/', include('authentication.urls')),
+    path('preferences/', include('userpreferences.urls'))
 ]
